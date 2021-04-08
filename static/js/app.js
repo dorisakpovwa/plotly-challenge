7620a87a9099d76f1456bd925feb69f073d14a93
dorisakpovwa <75711579+dorisakpovwa@users.noreply.github.com>
2021-04-08 15:52:30 -0400

    wip

--- static/js/app.js
+++ static/js/app.js
@@ -1,94 +1,84 @@
-// // Use D3.json() fetch to read the JSON file
-// // The data from the JSON file is arbitrarily named importedData as the argument
-// d3.json("data/samples.json").then((importedData) => {
-//     var bellydata = importedData;
-//    // console.log(bellydata);
-//      // Use D3 to select the dropdown menu
-//   var dropdownMenu = d3.select("#selDataset");
-//   // Assign the value of the dropdown menu option to a variable
-// //   var dataset = dropdownMenu.property("value");
-// subjectid = bellydata.names
-// subjectid.forEach(id => {
-//     dropdownMenu.append("option").text(id).property("value")
-// })
-// optionChanged(subjectid[0])
-
-// });
-// // Display an individual's demographic information
-// // Display each key value pair from the metadata JSON object somewhere on the page.
-// function demograph(nameid) {
-//     d3.json("data/samples.json").then((bellydata) => {
-//       var bellymeta = bellydata.metadata
-//       filterid = bellymeta.filter(bm => bm.id == nameid)
-//       // fetching the first element
-//       firstmeta = filterid[0]
-//       // referencing the sample metadata
-//       var demobox = d3.select("#sample-metadata")
-//       Object.entries(firstmeta).forEach(([key, value]) => {
-//           demobox.append("option").text(`${key}, ${value}`)
-//       })
-// })
-// }
-
-// function optionChanged(namesid) {
-//     demograph(namesid)
-// }
-/////
-// Use D3 to select the bar chart
-function init() {
-  // Grab a reference to the dropdown select element 
-  var selector = d3.select("#selDataset");
-  // Use the list of sample names to populate the select options
-  d3.json("data/samples.json").then((data) => {
-    var sampleNames = data.names;
-    sampleNames.forEach((sample) => {
-      selector.append("option").text(sample).property("value", sample);
-    });
-    // Use the first sample from the list to build the initial plots    
-    var firstSample = sampleNames[0]; buildCharts(firstSample); buildMetadata(firstSample);
+// Use D3.json() fetch to read the JSON file
+// The data from the JSON file is arbitrarily named importedData as the argument
+d3.json("data/samples.json").then((importedData) => {
+  var bellydata = importedData;
+  console.log(bellydata);
+  // Use D3 to select the dropdown menu 
+  var dropdownMenu = d3.select("#selDataset");
+  // Assign the value of the dropdown menu option to a variable
+  idnames = bellydata.names
+  idnames.forEach(id => {
+    dropdownMenu.append("option").text(id).property("value", id);
   });
+  //build initial plots with first sample on the list
+  optionChanged(idnames[0])
+
+});
+// Display an individual's demographic information
+// Display each key value pair from the metadata JSON object somewhere on the page.
+function demograph(person) {
+  d3.json("data/samples.json").then((bellydata) => {
+    bellymeta = bellydata.metadata;
+    // filter the data with the object with the desired sample number
+    filterid = bellymeta.filter(bm => bm.id == person);
+    // fetching the first element
+    firstmeta = filterid[0];
+    // referencing the sample metadata with d3 to select the box id
+    var demobox = d3.select("#sample-metadata");
+    // clear any existing metadata
+    demobox.html("");
+    // use object.entries to add each key and value pair to the box
+    // use d3 to append new tags for each key value in metadata
+    Object.entries(firstmeta).forEach(([key, value]) => {
+      demobox.append("option").text(`${key}, ${value}`)
+    })
+  })
 }
 
-// Initialize the dashboardinit();
-function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected  
-  buildMetadata(newSample);
-  buildCharts(newSample);
+function optionChanged(person) {
+  demograph(person)
 }
-
-// Demographics Panel
-function buildMetadata(sample) {
-  d3.json("data/samples.json").then((data) => {
-    var metadata = data.metadata;
-    // Filter the data for the object with the desired sample number    
-    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
-    var result = resultArray[0];
-    // Use d3 to select the panel with id of #sample-metadata    
-    var PANEL = d3.select("#sample-metadata");
-    // Use `.html("") to clear any existing metadata    
-    PANEL.html("");
-    // Use Object.entries to add each key and value pair to the panel    
-    // Hint: Inside the loop, you will need to use d3 to append new    
-    // tags for each key-value in the metadata.    
-    Object.entries(result).forEach(([key, value]) => {
-    PANEL.append("h6").text(`${key.toUpperCase()}: ${value}`);
-    });
-  });
-}
+/////////////
 
 
 // // 1. Create the buildChart function.
-// function buildCharts(sample) {  
-// // 2. Use d3.json to load the samples.json file   
-//   d3.json("data/samples.json").then((data) => {    
-// // 3. Create a variable that holds the samples array.     
-//   var samples = data.samples;
-// // 4. Create a variable that filters the samples for the object with the desired sample number.    
-//   var resultArray = samples.filter(sampleObj => sampleObj.id == sample);
+// function buildCharts(sample) {
+//   // 2. Use d3.json to load the samples.json file   
+//   d3.json("data/samples.json").then((bellydata) => {
+//     // 3. Create a variable that holds the samples array.     
+//     var samples = bellydata.samples;
+//     // 4. Create a variable that filters the samples for the object with the desired sample number.    
+//     var resultArray = samples.filter(sampleObj => sampleObj.otu_ids == sample);
+//     //  5. Create a variable that holds the first sample in the array.    
+//     var result = resultArray[0];
+//   });
+//   buildCharts(sample)
+// }
 
-// //  5. Create a variable that holds the first sample in the array.    
-//   var result = resultArray[0];
-// });}
+//cccccccccccccccccccccccccccccccccccccccccccccccc
+
+function Plot(sample) {
+  d3.json("data/samples.json").then((bellydata) => {
+    console.log(bellydata);
+    var samples = bellydata.samples;
+    var resultotu = samples.filter(sampleObj => sampleObj.otu_ids == sample_otu);
+    var samplev = samples.filter(sampleObj => sampleObj.sample_values == samplev);
+    var trace = {
+      type: "bar",
+      // mode: "lines",
+      // name: otu_labels,
+      // x: otu_ids,
+      // y: sample_values,
+      name: otu_labels,
+      x: resultotu,
+      y: samplev,
+      line: {
+        color: "#17BECF",
+      },
+    };
+  })
+  Plotly.newPlot("bar", [trace]);
+}
 
 
 //xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
